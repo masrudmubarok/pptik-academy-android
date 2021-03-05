8f068575a00e9cfa40ff6b0119f646b16bbe19c8
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-03-05 23:33:49 +0700

    Membuat layout dan activity Splash Screen

---
+++ app/src/main/java/com/mubarok/pptikacademy/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.mubarok.pptikacademy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread() {
+            public void run(){
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
