b160d6dd3b6a68ce0ab5f411176418faeff6939b
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-03-07 18:50:32 +0700

    Membuat layout halaman create account & sign in

--- app/src/main/java/com/mubarok/pptikacademy/CreateAccountActivity.java
+++ app/src/main/java/com/mubarok/pptikacademy/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.mubarok.pptikacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        //menerapkan tool bar sesuai id toolbar | ToolBarAtas adalah variabel buatan sndiri
+        Toolbar ToolBarAtasCreateAccount = (Toolbar)findViewById(R.id.toolbar_createaccount);
+        setSupportActionBar(ToolBarAtasCreateAccount);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
--- app/src/main/java/com/mubarok/pptikacademy/LoginActivity.java
+++ app/src/main/java/com/mubarok/pptikacademy/LoginActivity.java
@@ -1,7 +1,7 @@
 package com.mubarok.pptikacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +10,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //menerapkan tool bar sesuai id toolbar | ToolBarAtas adalah variabel buatan sndiri
+        Toolbar ToolBarAtas2 = (Toolbar)findViewById(R.id.toolbar_signin);
+        setSupportActionBar(ToolBarAtas2);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
