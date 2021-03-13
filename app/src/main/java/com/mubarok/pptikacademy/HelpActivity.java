8169de4d201dd5ef992c7c1fee85a66f009f5211
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-03-13 19:10:13 +0700

    Membuat layout & activity halaman Help

---
+++ app/src/main/java/com/mubarok/pptikacademy/HelpActivity.java
@@ -0,0 +1,21 @@
+package com.mubarok.pptikacademy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        //menerapkan tool bar sesuai id toolbar | ToolBarAtas adalah variabel buatan sndiri
+        Toolbar ToolBarAtasAccount = (Toolbar)findViewById(R.id.toolbar_help);
+        setSupportActionBar(ToolBarAtasAccount);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+}
