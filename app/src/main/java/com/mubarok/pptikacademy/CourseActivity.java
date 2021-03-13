c6bae1e435e0ae3986c0399de4bda5ca11ee45c5
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-03-13 12:15:02 +0700

    Update function button & manifest halaman account

--- app/src/main/java/com/mubarok/pptikacademy/CourseActivity.java
+++ app/src/main/java/com/mubarok/pptikacademy/CourseActivity.java
@@ -1,6 +1,9 @@
 package com.mubarok.pptikacademy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
@@ -13,6 +16,15 @@ public class CourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+
+        Button mBtn_account = findViewById(R.id.settingbutton);
+        mBtn_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iAccount = new Intent(getApplicationContext(),AccountActivity.class);
+                startActivity(iAccount);
+            }
+        });
 
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         TabItem tabMyCourses = findViewById(R.id.tabMyCourses);
