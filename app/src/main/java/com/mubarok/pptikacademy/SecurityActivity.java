c5d92a019ce0c1ffabdeb8e96ad19a20602a70ef
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-03-13 18:49:04 +0700

    Update layout & activity halaman Sign in & Security

--- app/src/main/java/com/mubarok/pptikacademy/SecurityActivity.java
+++ app/src/main/java/com/mubarok/pptikacademy/SecurityActivity.java
@@ -1,14 +1,36 @@
 package com.mubarok.pptikacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecurityActivity extends AppCompatActivity {
+
+    private Button mBtn_editS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_security);
+
+        //menerapkan tool bar sesuai id toolbar | ToolBarAtas adalah variabel buatan sndiri
+        Toolbar ToolBarAtasAccount = (Toolbar)findViewById(R.id.toolbar_securtiy);
+        setSupportActionBar(ToolBarAtasAccount);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //inialisasi button
+        mBtn_editS = (Button) findViewById(R.id.editbtnS);
+
+        //functin button
+//        mBtn_editS.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent iEditS = new Intent(getApplicationContext(),EditSecurityActivity.class);
+//                startActivity(iEditS);
+//            }
+//        });
     }
 }
