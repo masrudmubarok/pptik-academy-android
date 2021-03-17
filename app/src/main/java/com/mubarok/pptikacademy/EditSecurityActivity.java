269e988ed0c6a2bd4ea42008e80bef2be72c1d59
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-03-17 09:48:07 +0700

    Update layout & activity halaman Edit Security (Toolbar)

--- app/src/main/java/com/mubarok/pptikacademy/EditSecurityActivity.java
+++ app/src/main/java/com/mubarok/pptikacademy/EditSecurityActivity.java
@@ -1,6 +1,7 @@
 package com.mubarok.pptikacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class EditSecurityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_security);
+
+        //menerapkan tool bar sesuai id toolbar | ToolBarAtas adalah variabel buatan sndiri
+        Toolbar ToolBarAtasAccount = (Toolbar)findViewById(R.id.toolbar_editprofile);
+        setSupportActionBar(ToolBarAtasAccount);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
