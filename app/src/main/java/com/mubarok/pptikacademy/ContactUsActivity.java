013e4d39402296b8cbd288ceaf2bc8e1a4996694
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-03-20 00:35:51 +0700

    Update Contact us Activity

--- app/src/main/java/com/mubarok/pptikacademy/ContactUsActivity.java
+++ app/src/main/java/com/mubarok/pptikacademy/ContactUsActivity.java
@@ -2,10 +2,17 @@ package com.mubarok.pptikacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class ContactUsActivity extends AppCompatActivity {
+
+    private EditText mEditTextTo;
+    private EditText mEditTextSubject;
+    private EditText mEditTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,34 @@ public class ContactUsActivity extends AppCompatActivity {
         setSupportActionBar(ToolBarAtasAccount);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //inialisasi edit text
+        mEditTextTo = findViewById(R.id.edit_text_to);
+        mEditTextSubject = findViewById(R.id.edit_text_subject);
+        mEditTextMessage = findViewById(R.id.edit_text_message);
+
+        //inialisasi button
+        Button buttonSend = findViewById(R.id.btn_send);
+
+        //function button
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail() {
+        String recipientList = mEditTextTo.getText().toString();
+        String[] recipients = recipientList.split(",");
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
     }
 }
