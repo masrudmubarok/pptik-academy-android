b190b01b39f37a9c68593db84075597681fd5fda
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-03-19 23:52:03 +0700

    Update activity Profile & Security

--- app/src/main/java/com/mubarok/pptikacademy/SecurityActivity.java
+++ app/src/main/java/com/mubarok/pptikacademy/SecurityActivity.java
@@ -3,14 +3,35 @@ package com.mubarok.pptikacademy;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class SecurityActivity extends AppCompatActivity {
 
     private Button mBtn_editS;
+    HttpResponse httpResponse;
+    TextView mTxt_idP, mTxt_usernameS, mTxt_passwordS;
+    JSONObject jsonObject = null ;
+    String StringHolder = "" ;
+    // Adding HTTP Server URL to string variable.
+    String HttpURL = "http://192.168.1.25/pptik-academy-android/security-read.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +55,78 @@ public class SecurityActivity extends AppCompatActivity {
                 startActivity(iEditS);
             }
         });
+
+        // inisialisasi textview
+        mTxt_idP = (TextView)findViewById(R.id.textIdPD);
+        mTxt_usernameS = (TextView)findViewById(R.id.textUsernameSD);
+        mTxt_passwordS = (TextView)findViewById(R.id.textPasswordSD);
+
+        new GetDataFromServerIntoTextView(SecurityActivity.this).execute();
+    }
+
+    // Declaring GetDataFromServerIntoTextView method with AsyncTask.
+    public class GetDataFromServerIntoTextView extends AsyncTask<Void, Void, Void> {
+
+        // Declaring CONTEXT.
+        public Context context;
+
+        public GetDataFromServerIntoTextView(Context context)
+        {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            HttpClient httpClient = new DefaultHttpClient();
+
+            // Adding HttpURL to my HttpPost oject.
+            HttpPost httpPost = new HttpPost(HttpURL);
+
+            try {
+                httpResponse = httpClient.execute(httpPost);
+
+                StringHolder = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                // Passing string holder variable to JSONArray.
+                JSONArray jsonArray = new JSONArray(StringHolder);
+                jsonObject = jsonArray.getJSONObject(0);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+        protected void onPostExecute(Void result)
+        {
+            try {
+
+                // Adding JSOn string to textview after done loading.
+                mTxt_idP.setText(jsonObject.getString("id_siswa"));
+                mTxt_usernameS.setText(jsonObject.getString("username"));
+                mTxt_passwordS.setText(jsonObject.getString("password"));
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
