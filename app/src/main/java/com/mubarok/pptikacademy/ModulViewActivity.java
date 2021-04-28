d30fa5dcd3ca97ee16a4da7031527b46a06de414
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-04-28 18:51:27 +0700

    Membuat PDF View pada Activity & Layout Modul View

--- app/src/main/java/com/mubarok/pptikacademy/ModulViewActivity.java
+++ app/src/main/java/com/mubarok/pptikacademy/ModulViewActivity.java
@@ -2,12 +2,8 @@ package com.mubarok.pptikacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
-import org.apache.commons.io.IOUtils;
 import com.github.barteksc.pdfviewer.PDFView;
 
 import java.io.BufferedInputStream;
@@ -19,6 +15,7 @@ import java.net.URL;
 public class ModulViewActivity extends AppCompatActivity {
 
     PDFView pdfView;
+    String modulTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +29,19 @@ public class ModulViewActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        modulTemp = ("http://192.168.43.206/pptik-academy-web/assets/modul/modul-1-perngenalan-adobe-illustrator.pdf");
         pdfView = findViewById(R.id.pdfView);
 
-        //This is function read PDF from bytes
-        new RetrievePDFBytes().execute("http://192.168.43.206/pptik-academy-web/assets/modul/modul-1-perkenalan-adobe-illustrator.pdf");
+        //This is function read PDF from URL
+        new RetrievePDFStream().execute(modulTemp); // Or any url direct PDF from internet
+
     }
 
-    class RetrievePDFBytes extends AsyncTask<String,Void,byte[]>
+        class RetrievePDFStream extends AsyncTask<String,Void,InputStream>
     {
 
         @Override
-        protected byte[] doInBackground(String... strings) {
+        protected InputStream doInBackground(String... strings) {
             InputStream inputStream = null;
             try{
                 URL url = new URL(strings[0]);
@@ -56,17 +55,12 @@ public class ModulViewActivity extends AppCompatActivity {
             {
                 return null;
             }
-            try {
-                return IOUtils.toByteArray(inputStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+            return inputStream;
         }
 
         @Override
-        protected void onPostExecute(byte[] bytes) {
-            pdfView.fromBytes(bytes).load();
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream).load();
         }
     }
 }
